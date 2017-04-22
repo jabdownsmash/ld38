45b43d61f34f61a1683c0492124c10646284180f
Aaron Santiago <aaronsantiago@gmail.com>
2017-04-21 23:49:31 -0400

    make velocity scale with dt

--- components/velocity.js
+++ components/velocity.js
@@ -3,9 +3,9 @@ AFRAME.registerComponent('velocity', {
   tick: function(t, dt) {
     var position = this.el.getAttribute('position');
     this.el.setAttribute('position', {
-      x: position.x + this.data.x,
-      y: position.y + this.data.y,
-      z: position.z + this.data.z
+      x: position.x + this.data.x * dt / 1000,
+      y: position.y + this.data.y * dt / 1000,
+      z: position.z + this.data.z * dt / 1000
     });
   },
 });
