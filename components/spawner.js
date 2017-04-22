6145a231b869b009033db34d8e220bd69433c419
Aaron Santiago <aaronsantiago@gmail.com>
2017-04-21 23:16:13 -0400

    added hidden (broken) form of relative positioning for the spawner

--- components/spawner.js
+++ components/spawner.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent('spawner', {
   schema: {
     input:   {type: 'string'},
     mixin:   {type: 'string'},
-    parent:  {type: 'selector'}
+    parent:  {type: 'selector'},
+    relative: {type: 'boolean'},
   },
   init: function() {
     this.spawnFunction = this.spawn.bind(this);
@@ -16,6 +17,18 @@ AFRAME.registerComponent('spawner', {
       parent = this.data.parent;
     }
     parent.appendChild(newElement);
+
+    // relativePosition = null?
+    //if (this.data.relative) {
+    //  var relativePosition = newElement.getAttribute('position');
+    //  var position = this.el.getAttribute('position');
+
+    //  newElement.setAttribute('position', {
+    //    x: relativePosition.x + position.x,
+    //    y: relativePosition.y + position.y,
+    //    z: relativePosition.z + position.z,
+    //  });
+    //}
   }
 });
 
