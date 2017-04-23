616f2469a13acc9ed23c9c86f42242422ae0fbe8
Aaron Santiago <aaronsantiago@gmail.com>
2017-04-22 22:04:05 -0400

    change timer to be more functional and not break when tickRate = 0

--- components/timer.js
+++ components/timer.js
@@ -4,14 +4,16 @@ AFRAME.registerComponent('timer', {
     output: {type: 'string', default: 'timer'}
   },
   init: function() {
-    this.secondsPerTick = 1/this.data.tickRate;
     this.time = 0;
   },
   tick: function(t, dt) {
-    this.time += dt/1000;
-    while(this.time > this.secondsPerTick) {
-      this.time -= this.secondsPerTick;
-      this.el.emit(this.data.output, {tick:''});
+    if (this.data.tickRate) {
+      var secondsPerTick = 1/this.data.tickRate;
+      this.time += dt/1000;
+      while(this.time > secondsPerTick) {
+        this.time -= secondsPerTick;
+        this.el.emit(this.data.output, {tick:''});
+      }
     }
   }
 });
